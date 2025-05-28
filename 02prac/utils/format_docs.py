2c6b1993da7cd357869ad14d571cff1a9ba43e50
jjdj0315 <jjdj0315@gmail.com>
2025-05-28 19:09:51 +0900

    study : applying streamlit

--- 02prac/utils/format_docs.py
+++ 02prac/utils/format_docs.py
@@ -1,6 +1,12 @@
 # 문서 포맷팅
-def format_docs(docs):
-    return "\n\n".join(
-        f"<document><content>{doc.page_content}</content><page>{doc.metadata['page']}</page><source>{doc.metadata['source']}</source></document>"
-        for doc in docs
-    )
+# def format_docs(docs):
+#     return "\n\n".join(
+#         f"<document><content>{doc.page_content}</content><page>{doc.metadata['page']}</page><source>{doc.metadata['source']}</source></document>"
+#         for doc in docs
+#     )
+
+
+
+
+def split_documents(docs, splitter):
+    return [split for doc in docs for split in splitter.split_text(doc.page_content)]
