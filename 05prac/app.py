9f13a9f90791c67fa514474b64ba6e0a906dadb1
DAEJIN JEONG <jjdj0315@gmail.com>
2025-06-05 12:46:22 +0900

    study : 05prac print_message

---
+++ 05prac/app.py
@@ -0,0 +1,37 @@
+import streamlit as st
+
+from utils.uuid import random_uuid
+from utils.print_messages import print_messages
+
+st.title("LOCAL LLM RAG")
+st.markdown("OnPromise LLM RAG")
+
+# 사이드바 생성
+with st.sidebar:
+    clear_btn = st.button("대화 초기화")
+    st.markdown("# **made by JDJ**")
+    selected_loader = st.radio(
+        "로더 선택", ["docling", "PDFPlumber", "정대진"], index=0
+    )
+    file = st.file_uploader("pdf파일 업로드", type=["pdf"])
+    apply_btn = st.button("설정완료", type="primary")
+
+
+# 초기화 버튼
+if clear_btn:
+    st.session_state["messages"] = []
+    st.session_state["thread_id"] = random_uuid()
+    if "app" in st.session_state:
+        del st.session_state["app"]
+    if "compression_retriever" in st.session_state:
+        del st.session_state["compression_retriever"]
+    st.rerun()
+
+# 사용자 입력
+user_input = st.chat_input("궁금한 내용을 물어보세요")
+
+# 경고메시지 영역
+warning_msg = st.empty()
+
+# 이전 대화 기록 출력
+print_messages()
